b275799c2e7460429663743dae74d7807a5955f8
Gerard Rick Jardin <gerardrickjardin@Gerards-MacBook-Air.local>
2026-05-06 11:16:06 -0600

    Fix Vite index output

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        index: resolve(__dirname, 'index.html'),
         profile: resolve(__dirname, 'profile.html'),
         survey: resolve(__dirname, 'survey.html'),
         approach: resolve(__dirname, 'approach.html')
