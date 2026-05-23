4c05a1200424b939c7ae6487d46eed666e573bd4
Gerard Rick Jardin <gerardrickjardin@Gerards-Mac-mini.local>
2026-05-23 14:20:05 -0600

    Include hope-survey.html in the build script so Vercel deploys it properly

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ const path = require('path');
     fs.mkdirSync(outDir);
   }
 
-  const files = ['index.html', 'profile.html', 'survey.html', 'approach.html', 'how-to-use.html', 'rebuilding-life-challenges.html', 'employment-challenges.html', 'style.css', 'script.js'];
+  const files = ['index.html', 'profile.html', 'survey.html', 'approach.html', 'how-to-use.html', 'rebuilding-life-challenges.html', 'employment-challenges.html', 'style.css', 'script.js', 'hope-survey.html'];
 
   files.forEach(file => {
     if (fs.existsSync(file)) {
